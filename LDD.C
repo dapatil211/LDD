f971cc7dc5fa87e4a20901d8e459758dfd631403
Darshan Patil <dapatil211@gmail.com>
2018-05-03 09:51:31 -0400

    Parallel LDD done

--- LDD.C
+++ LDD.C
@@ -16,15 +16,17 @@ double exp(double lambda){
 struct LDD_UPDATE_F{
     double *S;
     double *C;
-    LDD_UPDATE_F(double *_S, double *_C) : S(_S), C(_C){}
+    double MAX_VALUE; 
+
+    LDD_UPDATE_F(double *_S, double *_C, double _MAX_VALUE) : S(_S), C(_C), MAX_VALUE(_MAX_VALUE){}
     inline bool update(uintE u, uintE v) {
-        if(C[2 * v + 1] ==  numeric_limits<double>::max()){
+        if(C[2 * v + 1] == MAX_VALUE){
             C[2 * v] = minF<double>()(S[u], C[2 * v]);
         }
         return false;
     }
     inline bool updateAtomic(uintE u, uintE v) {
-        if(C[2 * v + 1] ==  numeric_limits<double>::max()){
+        if(C[2 * v + 1] == MAX_VALUE){
             writeMin(C + 2 * v, S[u]);
         }
         return false;
@@ -50,28 +52,80 @@ struct LDD_CHECK_F{
         }
         return false;
     }
-    inline bool cond(uintE u){ true; }
+    inline bool cond(uintE u){ return true; }
 };
 
+struct CUT_EDGES_F {
+    uintE* IDs;
+    long &cut_edges;
+    CUT_EDGES_F(uintE* _IDs, long& _cut_edges) : 
+        IDs(_IDs), cut_edges(_cut_edges) {}
+
+    inline bool update(uintE s, uintE d){ //Update function writes min ID
+        if(IDs[s] != IDs[d] && s < d){
+            cut_edges ++;
+        }
+        return false;
+    }
+    inline bool updateAtomic (uintE s, uintE d) { //atomic Update
+        if(IDs[s] != IDs[d] && s < d){
+            writeAdd(&cut_edges, 1l);
+        }
+        return false;
+    }
+    inline bool cond (uintE d) { return cond_true(d); } //does nothing
+};
+
+struct CC_Vertex_F {
+    uintE* IDs;
+    CC_Vertex_F(uintE* _IDs) :
+        IDs(_IDs) {}
+    inline bool operator () (uintE i) {
+        if(IDs[i] == IDs[IDs[i]]){
+            return false;
+        } else{
+            if(i == IDs[IDs[i]]){
+                IDs[i] = min(IDs[i], i);
+            } else{
+                IDs[i] = IDs[IDs[i]];
+            }
+            return true;
+        }
+    }
+};
+
+void contract(long n, uintE* IDs){
+
+    bool* frontier = newA(bool,n);
+    {parallel_for(long i=0;i<n;i++) frontier[i] = 1;} 
+    vertexSubset Frontier(n,n,frontier); //initial frontier contains all vertices
+    while(!Frontier.isEmpty()){ //iterate until IDS converge
+        // cout << Frontier.size() << endl;
+        vertexSubset output = vertexFilter(Frontier,CC_Vertex_F(IDs));
+        Frontier.del();
+        Frontier = output;
+    }
+    Frontier.del();
+}
 
 template <class vertex>
 void Compute(graph<vertex>& GA, commandLine P){
     long n = GA.n;
     long m = GA.m;
-    double beta = P.getOptionDoubleValue("-b", .005);
+    double beta = P.getOptionDoubleValue("-e", .005);
     int procs = P.getOptionIntValue("-p", 4);
 
     setWorkers(procs);
     double *E = newA(double, n);
     double *S = newA(double, n);
     double *C = newA(double, 2 * n);
-    double MAX_VALUE = numeric_limits<double>::max                            (); 
-    cout << "Num Workers: " << getWorkers() << endl;
+    double MAX_VALUE = numeric_limits<double>::max(); 
+    cout << "N: " << n << " M: " << m << " Num Workers: " << getWorkers() << endl;
 
     {parallel_for(long i = 0; i < n; i++) E[i] = exp(beta);}
     double maxE = sequence::reduce(E, n, maxF<double>());
     {parallel_for(long i = 0; i < n; i++) {
-        S[i] = maxE - E[i]; 
+        S[i] = maxE - E[i];
     }}
     {parallel_for(long i = 0; i < 2 * n; i++) C[i] = MAX_VALUE;}
     
@@ -87,17 +141,42 @@ void Compute(graph<vertex>& GA, commandLine P){
         vertexSubset F_new(n, active);
         num_processed += F_new.size();
         F.del();
-        edgeMap(GA, F_new, LDD_UPDATE_F(S, C));
+        edgeMap(GA, F_new, LDD_UPDATE_F(S, C, MAX_VALUE));
         vertexSubset temp = edgeMap(GA, F_new, LDD_CHECK_F(S, C));
-        // cout << "TEMP : " << &temp << endl;
-        // cout << "F    : " << &F << endl;
-        // cout << "F_new: " << &F_new << endl;
         F_new.del();
         F = temp;
-        // cout << "TEMP : " << &temp << endl;
-        // cout << "F    : " << &F << endl;
-        // cout << "F_new: " << &F_new << endl;
         round ++;
     }
+    // cout << "ROUND: " << round << endl;
+    uintE *IDs = newA(uintE, n);
+    {parallel_for(long i = 0; i < n; i++){
+        if(C[2 * i + 1] == MAX_VALUE){
+            IDs[i] = i;
+        } else{
+            IDs[i] = (uintE) C[2 * i + 1];
+        }
+    }}
+    contract(n, IDs);
+    // for(int i =0; i < n; i++){
+    //     cout << "vertex: " << i << " cluster: " << IDs[i] << " START: " << S[i]<< endl;
+    // }
+    bool *uniq = newA(bool, n);
+    bool *verts = newA(bool, n);
+    {parallel_for(long i = 0; i < n; i++){
+        uniq[i] = 0;
+    }}
+    {parallel_for(long i = 0; i < n; i++){
+        uniq[IDs[i]] = 1;
+        verts[i] = 1;
+    }}
+    vertexSubset clusters(n, uniq);
+    long num_clusters = clusters.size();
+    clusters.del();
+
+    long cut_edges = 0;
+    vertexSubset allVerts(n, verts);
+    edgeMap(GA, allVerts, CUT_EDGES_F(IDs, cut_edges));
+
+    cout << "Beta: " << beta << " # Clusters: " << num_clusters << " # cut-edges: " << cut_edges << endl; 
 }
 
